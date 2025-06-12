d7907264409c57d7957a6796d308f715f47a3bf8
Michael Dowdle <michael.dowdle@hotmail.com>
2025-06-12 12:02:42 +0100

    Fix: use relative import paths for ClientLayout

--- app/(withSidebar)/layout.tsx
+++ app/(withSidebar)/layout.tsx
@@ -1,7 +1,7 @@
 // app/(withSidebar)/layout.tsx
 "use client";
 
-import ClientLayout from "@/app/(withSidebar)/ClientLayout";
+import ClientLayout from "./ClientLayout";
 
 export default function WithSidebarLayout({ children }: { children: React.ReactNode }) {
   return <ClientLayout>{children}</ClientLayout>;

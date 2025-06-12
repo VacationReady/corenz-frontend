468e10fe4c9eeb5a893ea885481b606e0a0b99f1
Michael Dowdle <michael.dowdle@hotmail.com>
2025-06-12 22:06:01 +0100

    fix: add Tailwind support by including globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+// /app/layout.tsx
+import "./globals.css";
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }

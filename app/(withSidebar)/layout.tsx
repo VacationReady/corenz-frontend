285db981b0ae2abac6806c5a10d069e73435ea5d
Michael Dowdle <michael.dowdle@hotmail.com>
2025-06-12 21:05:52 +0100

    refactor: restructure app layout and fix AdminSidebar path

--- app/(withSidebar)/layout.tsx
+++
@@ -1,9 +0,0 @@
-// app/(withSidebar)/layout.tsx
-"use client";
-
-import ClientLayout from "./ClientLayout";
-
-export default function WithSidebarLayout({ children }: { children: React.ReactNode }) {
-  return <ClientLayout>{children}</ClientLayout>;
-}
-

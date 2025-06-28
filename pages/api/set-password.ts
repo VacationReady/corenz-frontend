60a2b41b04bdc55617203d74e65c5315bded80e6
Michael Dowdle <michael.dowdle@hotmail.com>
2025-06-28 12:27:28 +0100

    Replace bcrypt with bcryptjs in set-password for Vercel build fix

--- pages/api/set-password.ts
+++ pages/api/set-password.ts
@@ -2,7 +2,7 @@
 
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/lib/prisma";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export default async function handler(
   req: NextApiRequest,

0fba20d282c147db929e3c17aa7036c0b4df71ef
Michael Dowdle <michael.dowdle@hotmail.com>
2025-06-28 12:32:18 +0100

    Correct NextAuth export for Pages Router compatibility

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,4 @@ import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth-options";
 import { prisma } from "@/lib/prisma";
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);

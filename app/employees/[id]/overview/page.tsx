c90f42230fc471362b3263ddb95035f45710c383
Michael Dowdle <michael.dowdle@hotmail.com>
2025-06-30 14:44:40 +0100

    Refactor: Update leaveEntitlement to leaveEntitlements throughout frontend

--- app/employees/[id]/overview/page.tsx
+++ app/employees/[id]/overview/page.tsx
@@ -79,7 +79,7 @@ export default async function EmployeeOverviewPage({ params }: PageProps) {
           </div>
           <div className="p-4 space-y-4">
             <LeaveBalancePanel
-              leaveEntitlements={employee.leaveEntitlement}
+              leaveEntitlements={employee.leaveEntitlements}
               employeeId={employee.id}
             />
 

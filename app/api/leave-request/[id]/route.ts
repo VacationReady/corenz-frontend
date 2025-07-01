975dbf8b1ba4e1198cea506a65aba5a9eb23b399
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-01 20:10:16 +0100

    api

--- app/api/leave-request/[id]/route.ts
+++ app/api/leave-request/[id]/route.ts
@@ -1,13 +1,12 @@
-// app/api/leave-request/[id]/route.ts
-
-import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth-options";
 import { sendLeaveStatusUpdate } from "@/lib/sendLeaveStatusUpdate";
 import calculateLeaveDeduction from "@/lib/calculateLeaveDeduction";
 
 export async function PATCH(req, { params }) {
-  const session = await auth();
+  const session = await getServerSession(authOptions);
   if (!session || !session.user || !session.user.email) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }

7c095ceed8894b318b174da4607ba814f474b30c
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-02 15:11:46 +0100

    uhuh

--- app/components/ui/switch.tsx
+++ app/components/ui/switch.tsx
@@ -13,15 +13,17 @@ export function Switch({ checked, onChange }: SwitchProps) {
     <HeadlessSwitch
       checked={checked}
       onChange={onChange}
+      as="button"
       className={cn(
-        "relative inline-flex h-6 w-11 items-center rounded-full",
+        "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out",
         checked ? "bg-blue-600" : "bg-gray-300"
       )}
     >
       <span
+        aria-hidden="true"
         className={cn(
-          "inline-block h-4 w-4 transform rounded-full bg-white transition",
-          checked ? "translate-x-6" : "translate-x-1"
+          "inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out",
+          checked ? "translate-x-5" : "translate-x-0"
         )}
       />
     </HeadlessSwitch>

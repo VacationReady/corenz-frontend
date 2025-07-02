0f571269649dab515a693e945ac9493be5d7718c
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-02 14:21:12 +0100

    asdad

--- app/components/ui/switch.tsx
+++ app/components/ui/switch.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import * as React from "react";
 import { Switch as HeadlessSwitch } from "@headlessui/react";
 import { cn } from "@/lib/utils";
-import React from "react";
 
 interface SwitchProps {
   checked: boolean;
@@ -15,16 +15,18 @@ export function Switch({ checked, onChange, className }: SwitchProps) {
     <HeadlessSwitch
       checked={checked}
       onChange={onChange}
+      as="button"
       className={cn(
-        "relative inline-flex h-6 w-11 items-center rounded-full transition-colors",
+        "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out",
         checked ? "bg-blue-600" : "bg-gray-300",
         className
       )}
     >
       <span
+        aria-hidden="true"
         className={cn(
-          "inline-block h-4 w-4 transform rounded-full bg-white transition-transform",
-          checked ? "translate-x-6" : "translate-x-1"
+          "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out",
+          checked ? "translate-x-5" : "translate-x-0"
         )}
       />
     </HeadlessSwitch>

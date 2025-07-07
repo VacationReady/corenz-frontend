2e0537f84af705824f3c558ff38bf5c5e87419e9
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-07 18:03:25 +0100

    asdad

--- app/employees/[id]/employment-checks/page.tsx
+++ app/employees/[id]/employment-checks/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
-import { Button } from '@/components/ui/Button';
+import Button from '@/components/ui/Button';
 import {
   Dialog,
   DialogTrigger,

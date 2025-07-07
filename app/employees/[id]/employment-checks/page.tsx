d35e6015e8833e0abdb6f8f22434dcd2114c927a
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-07 14:49:18 +0100

    fgjgyk

--- app/employees/[id]/employment-checks/page.tsx
+++ app/employees/[id]/employment-checks/page.tsx
@@ -20,7 +20,7 @@ import { format } from 'date-fns';
 
 export default function EmploymentChecksPage() {
   const params = useParams();
-  const employeeId = params.id as string;
+  const employeeId = typeof params?.id === "string" ? params.id : "";
 
   const [documents, setDocuments] = useState<any[]>([]);
   const [typeOfCheck, setTypeOfCheck] = useState('');

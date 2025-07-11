ca958bfe77669af4a8c1fc88c3a89eba3e809b78
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-11 11:50:16 +0100

    asda

--- app/components/dashboard/LeaveBalanceWidgetWrapper.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import LeaveBalanceWidget from "./LeaveBalanceWidget";
-
-export default function LeaveBalanceWidgetWrapper({ employeeId }: { employeeId: string }) {
-  return <LeaveBalanceWidget employeeId={employeeId} />;
-}

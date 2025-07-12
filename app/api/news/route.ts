330ebf0b306aba8a61ef5d70da59bf1275238de4
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-12 16:49:35 +0100

    asdas

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/lib/auth-options'
+import { authOptions } from '@/lib/auth-options'
 import { Resend } from 'resend'
 import slugify from 'slugify'
 

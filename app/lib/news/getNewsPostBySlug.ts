196bb7c4d07c74d422275025a1ea2f4312eb6f33
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-12 14:25:38 +0100

    asdas

---
+++ app/lib/news/getNewsPostBySlug.ts
@@ -0,0 +1,12 @@
+import { prisma } from '@/lib/prisma'
+
+export async function getNewsPostBySlug(slug: string) {
+  return prisma.newsPost.findUnique({
+    where: { slug },
+    include: {
+      author: {
+        select: { name: true, email: true },
+      },
+    },
+  })
+}

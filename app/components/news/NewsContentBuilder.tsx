c192af781987e037557e0f09227949c9aca25e60
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-12 22:19:51 +0100

    Button casing fix

--- app/components/news/NewsContentBuilder.tsx
+++ app/components/news/NewsContentBuilder.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Button } from '@/components/ui/Button';
+import Button from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
 import { Textarea } from '@/components/ui/textarea';
 import { Trash2, Plus } from 'lucide-react';

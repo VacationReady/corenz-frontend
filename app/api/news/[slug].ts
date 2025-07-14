5ef3ea0878cdbb319a3737719f7dbcd365a208d1
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-14 09:16:13 +0100

    asda

--- app/api/news/[slug].ts
+++ app/api/news/[slug].ts
@@ -55,7 +55,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       if (sendEmail) {
         const recipients = await prisma.user.findMany({
-          where: { email: { not: null } },
+          where: { email: { not: '' } },
           select: { email: true },
         })
 

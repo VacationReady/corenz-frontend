16abba865ea910635a7b5ea99822b9dad0bf800e
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-20 21:58:12 +0100

    asdad

--- app/news/create/page.tsx
+++ app/news/create/page.tsx
@@ -10,8 +10,8 @@ import { Switch } from '@/components/ui/switch'
 import { uploadFileToSupabase } from '@/lib/news/uploadFileToSupabase'
 import NewsContentBuilder from '@/components/news/NewsContentBuilder'
 import AudienceSelector from '@/components/news/AudienceSelector'
-import NewDepartmentModal from '@/app/employees/NewDepartmentModal'
-import NewJobRoleModal from '@/app/employees/NewJobRoleModal'
+import NewDepartmentModal from '@/app/(withSidebar)/employees/NewDepartmentModal'
+import NewJobRoleModal from '@/app/(withSidebar)/employees/NewJobRoleModal'
 
 type ContentBlock =
   | { type: 'heading'; level: number; text: string }

5636c6b69aceb7065b27fe7385811f804d40441a
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-21 08:18:04 +0100

    asdasd

--- app/api/audience/route.ts
+++ app/api/audience/route.ts
@@ -1,4 +1,7 @@
 // app/api/audience/route.ts
+
+export const dynamic = 'force-dynamic'
+
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'  // Adjust this path if needed
 

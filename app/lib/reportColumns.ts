ed4026c89ae856c47bc0c92d8de227d787967901
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-22 19:19:58 +0100

    asdasd

--- app/lib/reportColumns.ts
+++ app/lib/reportColumns.ts
@@ -1,8 +1,8 @@
 export const reportColumns = {
   leaveWithEmployee: [
-    { key: "employee.name", label: "Employee Name" },
-    { key: "employee.email", label: "Email" },
-    { key: "employee.department", label: "Department" },
+    { key: "employee.user.name", label: "Employee Name" },
+    { key: "employee.user.email", label: "Email" },
+    { key: "employee.department.name", label: "Department" },
     { key: "status", label: "Leave Status" },
     { key: "startDate", label: "Start Date" },
   ],

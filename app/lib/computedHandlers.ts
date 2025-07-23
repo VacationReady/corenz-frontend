8a782f5171e2750f5fe57c33e8d1ee45a44cb969
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-23 09:44:57 +0100

    asdsa

--- app/lib/computedHandlers.ts
+++ app/lib/computedHandlers.ts
@@ -97,7 +97,7 @@ export const computedHandlers: ComputedFieldRegistry = {
       const name = item.name || "";
       return name
         .split(" ")
-        .map((n) => n[0])
+        .map((n: string) => n[0])
         .join("")
         .toUpperCase();
     },

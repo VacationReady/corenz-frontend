3625f1a7f674839974b272f3d520ee204779f385
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-28 21:10:36 +0100

    asfsdf

--- app/api/documents/delete/route.ts
+++ app/api/documents/delete/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from "@/lib/auth-options";
-import { prisma from } '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 import supabase from "@/lib/supabase-admin";
 
 export async function DELETE(req: Request) {

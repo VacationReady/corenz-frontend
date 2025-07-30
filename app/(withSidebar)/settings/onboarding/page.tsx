5786bfea85e83ac42cd48da1a7dc5a2bb6b54863
Michael Dowdle <michael.dowdle@hotmail.com>
2025-07-30 19:14:11 +0100

    trivial

--- app/(withSidebar)/settings/onboarding/page.tsx
+++ app/(withSidebar)/settings/onboarding/page.tsx
@@ -107,14 +107,18 @@ export default function OnboardingSettingsPage() {
 
       {/* Modal for create/edit */}
       <Dialog open={isEditorOpen} onOpenChange={setIsEditorOpen}>
-        <DialogContent className="max-w-3xl p-0">
-          <OnboardingTemplateEditor
-            template={editingTemplate}
-            onSaved={() => { setIsEditorOpen(false); fetchTemplates(); }}
-            onCancel={() => setIsEditorOpen(false)}
-          />
-        </DialogContent>
-      </Dialog>
+  <DialogContent className="max-w-3xl p-0">
+    {isEditorOpen && (
+      <OnboardingTemplateEditor
+        // Add a key so state resets *only* when opening a new editor
+        key={editingTemplate?.id || "new"}
+        template={editingTemplate}
+        onSaved={() => { setIsEditorOpen(false); fetchTemplates(); }}
+        onCancel={() => setIsEditorOpen(false)}
+      />
+    )}
+  </DialogContent>
+</Dialog>
     </div>
   );
 }

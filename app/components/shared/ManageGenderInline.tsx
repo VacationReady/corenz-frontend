5add99517b6fd93bab73f7fb3773941c11440120
Michael Dowdle <michael.dowdle@hotmail.com>
2025-09-09 14:17:52 +0100

    yoyoyo

---
+++ app/components/shared/ManageGenderInline.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useState } from "react";
+import ManageGenderOptionsModal from "@/components/shared/ManageGenderOptionsModal";
+
+export default function ManageGenderInline() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="mt-2 text-right">
+      <button className="text-xs text-primary hover:underline" type="button" onClick={() => setOpen(true)}>
+        Manage options
+      </button>
+      {open && <ManageGenderOptionsModal onClose={() => setOpen(false)} />}
+    </div>
+  );
+}
+
+
